92d2d5d7072b26cbf6cca431ded85771e292cad8
Itamar Junior <itjunior@gmail.com>
2021-05-02 11:27:22 +0100

    EqualSumChecker

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,22 @@ public class Main {
 
 
 
+        //check BarkingDog
+        System.out.println("Barking "+BarkingDog.shouldWakeUp(true,9));
+
+        //Check DecimalOperator
+        System.out.println("Decimal "+DecimalComparator.areEqualByThreeDecimalPlaces(3.576,3.5769));
+
+        //check LeapYear
+        System.out.println("Leap "+LeapYear.isLeapYear(1999));
+
+        //Check MegaBytesConverter
+        MegaBytesConverter.printMegaBytesAndKiloBytes(1500);
+
+        //Check SpeedConverter
+        System.out.println("SpeedConverter "+SpeedConverter.toMilesPerHour(1500));
+
+        //Check EqualSumChecker
+        System.out.println("EqualSumChecker "+EqualSumChecker.hasEqualSum(1,1,1));
     }
 }
