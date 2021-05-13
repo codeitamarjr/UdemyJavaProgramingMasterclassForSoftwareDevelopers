eedc30617342321edb339931304f2b0322242a3e
Junior <itjunior@gmail.com>
2021-05-13 21:56:15 +0100

    isCatPlaying

--- src/PlayingCat.java
+++ src/PlayingCat.java
@@ -1,19 +1,9 @@
 public class PlayingCat {
     public static boolean isCatPlaying( boolean isSummer, int temperature){
-<<<<<<< HEAD
         if(temperature >= 25 & temperature <=35 & !isSummer){
             return true;
         } else if(temperature >= 25 & temperature <= 45 & isSummer){
             return true;
         } else return false;
-=======
-        if(temperature >= 25 & temperature <=35 & isSummer == false){
-            return true;
-        } else if(temperature >= 25 & temperature <= 45 & isSummer == true){
-            return true;
-        } else {
-            return false;
-        }
->>>>>>> origin/master
     }
 }
